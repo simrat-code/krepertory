8c48c8495152f7a36f0f06387a5b452887e9e386
Simrat Singh <simrat.khokhar@gmail.com>
2021-07-11 01:48:30 +0530

    UI and rubric add-remove flow is working

--- krepertory.py
+++ krepertory.py
@@ -1,15 +1,28 @@
 
-from rubrics import iDefault
-from rubrics import rubricsdb
-
+import random
+import string
+from flask.helpers import flash
+from flask.helpers import url_for
+from flask.templating import render_template
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import session
+from flask import redirect
+
+from rubrics import iDefault
+from rubrics import rubricsdb
 
 
 rublist = [rub for rub in rubricsdb.keys()]
 
 app = Flask(__name__)
+# key for session
+# app.secret_key = "Ph38fTX3Krztt9NFy9zUoe71UivbYvI9"     # need to CHANGE it to random generation
+app.secret_key = ''.join(random.SystemRandom().choice(
+    string.ascii_uppercase + string.digits + string.ascii_lowercase) 
+    for _ in range(32)
+    )
 
 @app.errorhandler(404)
 def pageNotFound(e):
@@ -30,11 +43,45 @@ def rubricsMeds():
     rub = request.args['name']
     return jsonify(rubricsdb[rub])
 
+@app.route('/')
+def home():
+    if "selected" not in session.keys():
+        session['selected'] = []
+        print('selected added')
+    return render_template('home.html',
+        selected = session['selected'],
+        available = rublist, 
+        skey=app.secret_key
+        )
+
+@app.route('/api/v1/addRubric/<string:rub>')
+def addRubric(rub):
+    if "selected" not in session.keys():
+        flash('Server resetted, kindly re-select', 'error')
+        return redirect(url_for('home'))
+    if rub not in session['selected']: 
+        session['selected'].append(rub)
+        session.modified = True
+        flash("added: "+ rub, category='info')
+    else:
+        flash("already selected: "+ rub, category='warning')
+    return redirect(url_for('home'))
+
+@app.route('/api/v1/removeRubric/<string:rub>')
+def removeRubric(rub):
+    if "selected" not in session.keys():
+        flash('Server resetted, kindly re-select', 'error')
+        return redirect(url_for('home'))
+    session['selected'].remove(rub)
+    session.modified = True
+    flash("removed: "+ rub, category='info')
+    return redirect(url_for('home'))
+
 
 if __name__ == "__main__":
     app.run(
         host="127.0.0.1",
-        port=50001,
+        port=5001,
         debug=True
     )
     # done
